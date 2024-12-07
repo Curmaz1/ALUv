0218a5776ea0e7c2f9c80e887ad3e1a8bd1fdfdf
chutse <jesusesparza242@gmail.com>
2024-12-07 16:36:26 -0600

    covergroups html added

--- vector_alu_tb.sv
+++ vector_alu_tb.sv
@@ -1,8 +1,38 @@
+`define CREATE_COVERGROUP_BASIC(name) \
+    covergroup name; \
+        cp_a0 : coverpoint alu_if_inst.A[0] { option.auto_bin_max = bins_nums; } \
+        cp_a1 : coverpoint alu_if_inst.A[1] { option.auto_bin_max = bins_nums; } \
+        cp_a2 : coverpoint alu_if_inst.A[2] { option.auto_bin_max = bins_nums; } \
+        cp_a3 : coverpoint alu_if_inst.A[3] { option.auto_bin_max = bins_nums; } \
+        cp_b0 : coverpoint alu_if_inst.B[0] { option.auto_bin_max = bins_nums; } \
+        cp_b1 : coverpoint alu_if_inst.B[1] { option.auto_bin_max = bins_nums; } \
+        cp_b2 : coverpoint alu_if_inst.B[2] { option.auto_bin_max = bins_nums; } \
+        cp_b3 : coverpoint alu_if_inst.B[3] { option.auto_bin_max = bins_nums; } \
+    endgroup
+
+`define CREATE_COVERGROUP_WITH_SEL(name) \
+    covergroup name; \
+        cp_a0 : coverpoint alu_if_inst.A[0] { option.auto_bin_max = bins_nums; } \
+        cp_a1 : coverpoint alu_if_inst.A[1] { option.auto_bin_max = bins_nums; } \
+        cp_a2 : coverpoint alu_if_inst.A[2] { option.auto_bin_max = bins_nums; } \
+        cp_a3 : coverpoint alu_if_inst.A[3] { option.auto_bin_max = bins_nums; } \
+        cp_b0 : coverpoint alu_if_inst.B[0] { option.auto_bin_max = bins_nums; } \
+        cp_b1 : coverpoint alu_if_inst.B[1] { option.auto_bin_max = bins_nums; } \
+        cp_b2 : coverpoint alu_if_inst.B[2] { option.auto_bin_max = bins_nums; } \
+        cp_b3 : coverpoint alu_if_inst.B[3] { option.auto_bin_max = bins_nums; } \
+        cp_sel0 : coverpoint alu_if_inst.sel[0] { option.auto_bin_max = bins_nums; } \
+        cp_sel1 : coverpoint alu_if_inst.sel[1] { option.auto_bin_max = bins_nums; } \
+        cp_sel2 : coverpoint alu_if_inst.sel[2] { option.auto_bin_max = bins_nums; } \
+        cp_sel3 : coverpoint alu_if_inst.sel[3] { option.auto_bin_max = bins_nums; } \
+    endgroup
+
+
 module tb_vector_alu;
 
     // Parámetros
     parameter N = 4;     // Número de ALUs internas
     parameter WIDTH = 8; // Ancho de las entradas y salidas
+    integer bins_nums = 256; // Numero de Bins en los Coverpoints
 
     // Instanciar la interface
     alu_if #(.N(N), .WIDTH(WIDTH)) alu_if_inst();
@@ -17,104 +47,132 @@ module tb_vector_alu;
         .enable(alu_if_inst.enable),
         .Z(alu_if_inst.Z)
     );
-
-//`define TEST1
-`define TEST2
-//`define TEST3
-//`define TEST4
-//`define TEST5
+    
+    //`define TEST1
+    //`define TEST2
+    //`define TEST3
+    `define TEST4
 
     initial alu_if_inst.clk = 0;
-    always #1 alu_if_inst.clk = ~alu_if_inst.clk;
+    always #25ps alu_if_inst.clk = ~alu_if_inst.clk;
 
-`ifdef TEST1
-    initial begin
-        // Reset 
-        alu_if_inst.arst = 1;
-        #1;
-        alu_if_inst.arst = 0;
-
-        repeat (1000) begin 
-            @(posedge alu_if_inst.clk);
-                alu_if_inst.generate_random_stimuli(); 
+    `ifdef TEST1
+        `CREATE_COVERGROUP_BASIC(cg_inputs)
+        initial begin
+            cg_inputs cg_inputs_inst = new();
+            // Reset 
+            alu_if_inst.arst = 1;
+            #1ns;
+            alu_if_inst.arst = 0;
+            
+            repeat (2000) begin 
+                @(posedge alu_if_inst.clk);
+                    alu_if_inst.generate_random_stimuli();
+                    cg_inputs_inst.sample();
+            end
         end
-    end
 
-`elsif TEST2
-    initial begin
+    `elsif TEST2
+        `CREATE_COVERGROUP_BASIC(cg_add_inputs)
+        `CREATE_COVERGROUP_BASIC(cg_sub_inputs)
+        `CREATE_COVERGROUP_BASIC(cg_and_inputs)
+        `CREATE_COVERGROUP_BASIC(cg_or_inputs)
+        `CREATE_COVERGROUP_BASIC(cg_mul_inputs)
+        `CREATE_COVERGROUP_BASIC(cg_div_inputs)
 
-        alu_if_inst.arst = 1;
-        #10;
-        alu_if_inst.arst = 0;
+        initial begin
+            cg_add_inputs cg_add_inputs_i = new();
+            cg_sub_inputs cg_sub_inputs_i = new();
+            cg_and_inputs cg_and_inputs_i = new();
+            cg_or_inputs cg_or_inputs_i = new();
+            cg_mul_inputs cg_mul_inputs_i = new();
+            cg_div_inputs cg_div_inputs_i = new();
+            
+            alu_if_inst.arst = 1;
+            #1ns;
+            alu_if_inst.arst = 0;
 
-        repeat (70) begin
-            @(posedge alu_if_inst.clk);
-                alu_if_inst.add_a_b_random();             
+            repeat (2000) begin
+                @(posedge alu_if_inst.clk);
+                    alu_if_inst.add_a_b_random(); 
+                    cg_add_inputs_i.sample();        
+            end
+            repeat (2000) begin
+                @(posedge alu_if_inst.clk);
+                    alu_if_inst.sub_a_b_random();
+                    cg_sub_inputs_i.sample();             
+            end
+            repeat (2000) begin
+                @(posedge alu_if_inst.clk);
+                    alu_if_inst.and_a_b_random();
+                    cg_and_inputs_i.sample();             
+            end
+            repeat (2000) begin
+                @(posedge alu_if_inst.clk);
+                    alu_if_inst.or_a_b_random();
+                    cg_or_inputs_i.sample();             
+            end
+            repeat (2000) begin
+                @(posedge alu_if_inst.clk);
+                    alu_if_inst.mul_a_b_random();
+                    cg_mul_inputs_i.sample();             
+            end        
+            repeat (2000) begin
+                @(posedge alu_if_inst.clk);
+                    alu_if_inst.div_a_b_random();
+                    cg_div_inputs_i.sample();             
+            end        
         end
-        repeat (70) begin
-            @(posedge alu_if_inst.clk);
-                alu_if_inst.sub_a_b_random();             
-        end
-        repeat (70) begin
-            @(posedge alu_if_inst.clk);
-                alu_if_inst.and_a_b_random();             
-        end
-        repeat (70) begin
-            @(posedge alu_if_inst.clk);
-                alu_if_inst.or_a_b_random();             
-        end
-        repeat (70) begin
-            @(posedge alu_if_inst.clk);
-                alu_if_inst.mul_a_b_random();             
-        end        
-        repeat (70) begin
-            @(posedge alu_if_inst.clk);
-                alu_if_inst.div_a_b_random();             
-        end        
-    end
 
-`elsif TEST3
-    initial begin
+    `elsif TEST3
+        initial begin
+            ////////////////////////////////////////////////
+            // 1. Generar 20 sumas con ceros              //
+            // 2. Generar 20 restas con ceros             //
+            //   3. Generar 20 multiplicaciones con ceros //                   
+            ////////////////////////////////////////////////
+            repeat (20) begin
+                @(posedge alu_if_inst.clk);
+                    alu_if_inst.add_a_b_zero();
+            end
+            repeat (20) begin
+                @(posedge alu_if_inst.clk);
+                    alu_if_inst.sub_a_b_zero();
+            end
+
+            repeat (20) begin
+                @(posedge alu_if_inst.clk);
+                    alu_if_inst.mul_a_b_zero();
+            end
+        end
+        
+        
+    `elsif TEST4
+        // Prueba 4 - 
         ////////////////////////////////////////////////
-        // 1. Generar 20 sumas con ceros              //
-        // 2. Generar 20 restas con ceros             //
-        //   3. Generar 20 multiplicaciones con ceros //                   
+        // 1. Generar  operaciones con a = 0       //                   
+        // 2. Generar  operaciones con b = 0      //
         ////////////////////////////////////////////////
-        repeat (20) begin
-            @(posedge alu_if_inst.clk);
-                add_a_b_zero();
-        end
-        repeat (20) begin
-            @(posedge alu_if_inst.clk);
-                sub_a_b_zero();
-        end
+        `CREATE_COVERGROUP_WITH_SEL(cg_a_zero_b_op_random)
+        `CREATE_COVERGROUP_WITH_SEL(cg_b_zero_a_op_random)
+        
+        initial begin
+            cg_a_zero_b_op_random cg_a_zero_b_op_random_i = new();
+            cg_b_zero_a_op_random cg_b_zero_a_op_random_i = new();
+            
+            repeat (2000) begin
+                @(posedge alu_if_inst.clk);  
+                    alu_if_inst.a_zero_b_op_random();   
+                    cg_a_zero_b_op_random_i.sample();         
+            end
+            repeat (2000) begin
+                @(posedge alu_if_inst.clk); 
+                    alu_if_inst.b_zero_a_op_random(); 
+                    cg_b_zero_a_op_random_i.sample();             
+            end        
 
-        repeat (20) begin
-            @(posedge alu_if_inst.clk);
-                mul_a_b_zero();
         end
-    end
-    
-    
-`elsif TEST4
-    // Prueba 4 - 
-    ////////////////////////////////////////////////
-    // 1. Generar 300 operaciones con a = 0       //                   
-    // 2. Generar 3000 operaciones con b = 0      //
-    ////////////////////////////////////////////////
-    initial begin
-        repeat (300) begin
-            @(posedge ALU_intf_i.clk);  
-            ALU_intf_i.a_zero_b_op_random();            
-        end
-        repeat (3000) begin
-            @(posedge ALU_intf_i.clk); 
-                ALU_intf_i.b_zero_a_op_random();              
-        end        
-        //@(posedge ALU_intf_i.clk); 
-            //$finish;     
-    end
-`endif
+    `endif
 
 
 endmodule
@@ -128,7 +186,7 @@ interface alu_if #(parameter N = 4, WIDTH = 8);
     logic [2:0] sel [N-1:0];
     logic [N-1:0] enable;
     logic [WIDTH*2-1:0] Z [N-1:0];
- 
+   
     task automatic generate_random_stimuli();
         assert(std::randomize(A) with {
             foreach (A[i]) A[i] inside {[0:255]};});
@@ -158,7 +216,7 @@ interface alu_if #(parameter N = 4, WIDTH = 8);
             assert(std::randomize(B) with {
                 foreach (B[i]) B[i] inside {[0:255]};});
 
-            foreach (sel[i]) sel[i] = 3'b001; // Suma
+            foreach (sel[i]) sel[i] = 3'b001; // sub
         end
     endtask    
     task automatic and_a_b_random();
@@ -169,7 +227,7 @@ interface alu_if #(parameter N = 4, WIDTH = 8);
             assert(std::randomize(B) with {
                 foreach (B[i]) B[i] inside {[0:255]};});
 
-            foreach (sel[i]) sel[i] = 3'b010; // Suma
+            foreach (sel[i]) sel[i] = 3'b010; // and
         end
     endtask    
     task automatic or_a_b_random();
@@ -180,7 +238,7 @@ interface alu_if #(parameter N = 4, WIDTH = 8);
             assert(std::randomize(B) with {
                 foreach (B[i]) B[i] inside {[0:255]};});
 
-            foreach (sel[i]) sel[i] = 3'b011; // Suma
+            foreach (sel[i]) sel[i] = 3'b011; // Or
         end
     endtask    
     task automatic mul_a_b_random();
@@ -191,7 +249,7 @@ interface alu_if #(parameter N = 4, WIDTH = 8);
             assert(std::randomize(B) with {
                 foreach (B[i]) B[i] inside {[0:255]};});
 
-            foreach (sel[i]) sel[i] = 3'b100; // Suma
+            foreach (sel[i]) sel[i] = 3'b100; // Mul
         end
     endtask    
     task automatic div_a_b_random();
